23b589ad014d0b2cb497b797c28ff6caceee89fe
MagicalInnkeep <TheMagicalInnkeeper@proton.me>
2025-01-14 21:18:44 +0100

    Implement basic flow
    Core function of application is implemented.
    We have most of the dom manipulations setup for
    the creation of div's.
    We've got a semblance of css setup with flexbox.
    We have our first eventHandler set to be able to
    draw.

--- app.js
+++ app.js
@@ -2,10 +2,38 @@
 function setupMenu(gridXY,brushMode,brushColor){
     const body=document.querySelector("body");
 
+//---
+
+    //Create FlexBox Container
+    const container=document.createElement("div");
+    container.setAttribute("class","container");
+    body.appendChild(container)
+//---   
+
+    // Create a Header
+    const headerDiv = document.createElement("div");
+    headerDiv.setAttribute("class","header");
+    container.appendChild(headerDiv);
+    const header1 = document.createElement("h1");
+    header1.setAttribute("class","mainHeader");
+    header1.textContent="Etch-a-Sketch"
+    headerDiv.appendChild(header1);
+
+//---
+
+    // Create a appContainer
+    const appContainer = document.createElement("div");
+    appContainer.setAttribute("class","appContainer");
+    container.appendChild(appContainer);
+
+//---
+
     // Create a div for the menu
     const menuDiv = document.createElement("div");
     menuDiv.setAttribute("class","menu");
-    body.appendChild(menuDiv);
+    appContainer.appendChild(menuDiv);
+
+//---
 
     // Create a div, a label and input for gridXY
     const menuGridXY = document.createElement("div");
@@ -15,15 +43,15 @@ function setupMenu(gridXY,brushMode,brushColor){
     const lblGridXY = document.createElement("p");
     lblGridXY.setAttribute("class","lblGridXY");
     lblGridXY.textContent=`Grid size: ${gridXY}x${gridXY}`;
-    menuDiv.appendChild(lblGridXY);
+    menuGridXY.appendChild(lblGridXY);
 
     const inputGridXY = document.createElement("input");
     inputGridXY.setAttribute("class","inputGridXY");
     inputGridXY.setAttribute("type","range");
-    inputGridXY.setAttribute("id","gridXY");
+    inputGridXY.setAttribute("id","inputGridXY");
     inputGridXY.setAttribute("min","1");
     inputGridXY.setAttribute("max","64");
-    menuDiv.appendChild(inputGridXY);
+    menuGridXY.appendChild(inputGridXY);
 
     // Create a button for brushMode
 
@@ -31,7 +59,31 @@ function setupMenu(gridXY,brushMode,brushColor){
 }
 
 // Create a grid
+function setupGrid(gridXY,brushColor){
+    const appContainer= document.querySelector(".appContainer");
 
+    const etchASketch = document.createElement("div");
+    etchASketch.setAttribute("class","etchASketch");
+    appContainer.appendChild(etchASketch);
+
+    for(var i=0;i < gridXY; i++){
+        for(var j=0; j<gridXY; j++){
+            const divGrid = document.createElement("div");
+            divGrid.setAttribute("id",`grid${i}x${j}`);
+            divGrid.setAttribute("style","height:"+100/gridXY+"%;width:"+100/gridXY+"%;");
+            divGrid.addEventListener("click",() =>{
+                colorGrid(gridXY, divGrid, brushColor);
+            });
+            etchASketch.appendChild(divGrid);
+        }
+    }
+
+}
+
+function colorGrid(gridXY, divGrid, brushColor){
+    console.log(divGrid.id+" and color "+brushColor);
+    divGrid.setAttribute("style","height:"+100/gridXY+"%;width:"+100/gridXY+"%;background-color:"+brushColor+";");
+}
 // Clean the grid
 
 // Define the brush size
@@ -52,7 +104,7 @@ function main(gridXY=16,brushMode=0,brushColor="black"){
     // Populate Menu with all the defined variables
     setupMenu(gridXY,brushMode,brushColor);
     // Draw the grid.
-    //setupGrid(gridXY);
+    setupGrid(gridXY,brushColor);
 }
 
 main();
